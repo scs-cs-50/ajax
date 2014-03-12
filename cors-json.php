0fa6062a30ae132a2e350a456ed4ca736f30cfc6
Scotty Logan <swl@stanford.edu>
2014-03-11 19:48:13 -0700

    added Authorization header support

--- cors-json.php
+++ cors-json.php
@@ -1,4 +1,6 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: http://scs-cs-50.github.io');
+// headers that are allowed in requests
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 ?>{"message":"Hello, World!"}
